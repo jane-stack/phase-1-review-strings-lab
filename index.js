95e0c25f01170abb20d06253bceb89bcfe465839
Tuyen Ha <tuyen_ha@outlook.com>
2022-03-14 10:17:53 -0600

    commit tree b8a9f3b569de5319498182a77897257e6c441b60

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Jane Nguyen';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(`Welcome to Flatbook, ${currentUser}!`);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
